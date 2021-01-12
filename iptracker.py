12ab298e1488b9a26e98868e9c7318572454247c
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-01-12 04:16:05 +0000

    Add files via upload

---
+++ iptracker.py
@@ -0,0 +1,22 @@
+#Author:iFanpS
+#Credit:Thx to GuckTubeYT
+import os
+import urllib.request as urllib2
+import json
+
+while True:
+	print('[+] ==================')
+	ip = input('Put ur target: ')
+	url = "http://ip-api.com/json/"
+	konz = urllib2.urlopen(url + ip)
+	data = konz.read()
+	irfan = json.loads(data)
+
+	print("IP: " + irfan['query'])
+	print("City: " + irfan['city'])
+	print("Status: " + irfan['region'])
+	print("ISP: " + irfan['isp'])
+	print("TimeZone: " + irfan['timezone'])
+	print('[+] ==================')
+
+	break
